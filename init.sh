89bea71f63c9bfd66e48e1611391c7daf47e6d8f
loomanss <stijnloomans@live.be>
2021-01-14 18:59:19 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # install ssh server
 sudo apt update -y  && sudo apt upgrade -y
-sudo apt install openssh-server
-sudo systemctl status ssh
-sudo ufw allow ssh
+sudo apt install openssh-server -y
+sudo systemctl status ssh -y
+sudo ufw allow ssh -y
 
 # install ansible
 sudo apt install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
-apt-get update
+apt-get update -y
 apt-get install ansible -y
