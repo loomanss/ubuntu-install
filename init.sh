ebf25406b17dd99b13838f70a35adf469f2c7f99
loomanss <stijnloomans@live.be>
2021-01-14 19:08:49 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ sudo apt install openssh-server -y
 sudo systemctl status ssh -y
 sudo ufw allow ssh -y
 
+#install git
+sudo apt install git
+
 # install ansible
 sudo apt install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
