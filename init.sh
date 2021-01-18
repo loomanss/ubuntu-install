9aafadcd23866f17b1b278488929dd25e12887c0
loomanss <stijnloomans@live.be>
2021-01-18 19:08:10 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 # install ssh server
+echo update & upgrade
+echo -------------------------------------------------------------------------
 sudo apt update && sudo apt upgrade -y
 sudo ufw allow ssh 
+echo
+echo
+echo installing prerequisites
+echo -------------------------------------------------------------------------
+sudo apt install software-properties-common git openssh-server  -y
 
-
-# install ansible repo
-sudo apt install software-properties-common -y
+echo 
+echo
+echo  add repo key
+echo -------------------------------------------------------------------------
 sudo apt-add-repository ppa:ansible/ansible -y
+echo  updating repo
 sudo apt-get update
 
-echo INSTALL PREREQUISITES : git openssh-server
-sudo apt-get install  git openssh-server  
-
-
-
+echo INSTALL ansible
+echo -------------------------------------------------------------------------
 sudo apt-get install ansible -y
 # install community collections
 # ansible-galaxy collection install community.general
 # ansible-galaxy collection install community.docker
-
+echo 
+echo download playbook repo
+echo -------------------------------------------------------------------------
 sudo mkdir /github/
 cd /github/
 #sudo wget https://github.com/loomanss/ubuntu-install/archive/main.zip
 sudo git clone https://github.com/loomanss/ubuntu-install.git
-sudo cd /github/ubuntu-install
+cd /ubuntu-install/playbooks/
