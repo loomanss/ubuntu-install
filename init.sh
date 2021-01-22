41b0cbd445b3819be1c319d350664256a109a391
loomanss <stijnloomans@live.be>
2021-01-22 15:22:04 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,37 +13,32 @@ echo -------------------------------------------------------------------------
 sudo apt install software-properties-common git openssh-server  -y
 
 
-git config --global user.email $1
-git config --global user.name $2
+git config --global user.email "$1"
+git config --global user.name "$2"
 
-echo 
-echo
-echo  add repo key
-echo -------------------------------------------------------------------------
-# sudo apt-add-repository ppa:ansible/ansible -y
-echo
-echo
-echo  updating repo
-echo -------------------------------------------------------------------------
-sudo apt-get update
 
+echo
+echo
 echo INSTALL ansible
 echo -------------------------------------------------------------------------
 sudo apt-get install ansible -y
-
+echo
+echo
 echo INSTALL ansible galaxy community.general 
 echo -------------------------------------------------------------------------
 ansible-galaxy collection install community.general
-
+echo
+echo
 echo INSTALL ansible galaxy community.docker 
 echo -------------------------------------------------------------------------
 ansible-galaxy collection install community.docker
 
+echo
+echo
 echo 
 echo download playbook repo
 echo -------------------------------------------------------------------------
 sudo mkdir -p /share/github
 cd /share/github
-#sudo wget https://github.com/loomanss/ubuntu-install/archive/main.zip
 sudo git clone https://github.com/loomanss/ubuntu-install.git
 cd /share/github/ubuntu-install/playbooks/
