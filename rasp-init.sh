8bbc0dd728d75f5d2dc1ea3739782d4c4a49bfd7
loomanss <stijnloomans@live.be>
2021-01-26 16:08:02 +0100

    Update rasp-init.sh

--- rasp-init.sh
+++ rasp-init.sh
@@ -11,3 +11,8 @@ echo
 echo installing prerequisites
 echo -------------------------------------------------------------------------
 sudo apt install software-properties-common git openssh-server  -y
+echo
+echo
+echo extend lifetime os SD card by disabling swap files
+echo -------------------------------------------------------------------------
+sudo systemctl disable dphys-swapfile.service
