ec2ee4b5821b822c486864e26845587260002d80
loomanss <stijnloomans@live.be>
2021-06-29 15:20:45 +0200

    Update ubuntu_vault_init.sh

--- ubuntu_vault_init.sh
+++ ubuntu_vault_init.sh
@@ -15,12 +15,22 @@ read bwusername
 export BW_SESSION=$(bw login $bwusername --raw) 
 echo "type "bw unlock"
 echo "bw get password google"
-bw unlock
+
+export BW_SESSION=$(bw unlock --raw)
 
 
+bw list items | jq -c '.[] |select(.id|contains("53d2339c-70a1-4ed3-a077-aaf100a470bd")) | .notes' >> ~/.ssh/id_rsa.b64
+ sed -ie 's/^"//'  ~/.ssh/id_rsa.b64
+ sed -ie  's/"$//' ~/.ssh/id_rsa.b64
+openssl base64  -A -d -in ~/.ssh/id_rsa.b64 -out ~/.ssh/id_rsa
+cat  ~/.ssh/id_rsa
 
-bw list items | jq -c '.[] |select(.id|contains("53d2339c-70a1-4ed3-a077-aaf100a470bd")) | .notes' >> ~/.ssh/rsa_id
-bw list items | jq -c '.[] |select(.id|contains("8e383309-94f8-4fbb-b381-ab4400caaf33")) | .notes' >> ~/.ssh/rsa_id.pub
-chmod 700 ~/.ssh
+
+bw list items | jq -c '.[] |select(.id|contains("8e383309-94f8-4fbb-b381-ab4400caaf33")) | .notes' >> ~/.ssh/id_rsa.pub.b64
+ sed -ie 's/^"//'  ~/.ssh/id_rsa.pub.b64
+ sed -ie  's/"$//' ~/.ssh/id_rsa.pub.b64
+openssl base64  -A -d -in ~/.ssh/id_rsa.pub.b64 -out ~/.ssh/id_rsa.pub
+cat  ~/.ssh/id_rsa.pub
+
 chmod 600 ~/.ssh/*
 chmod 644 -f ~/.ssh/*.pub ~/.ssh/authorized_keys ~/.ssh/known_hosts
