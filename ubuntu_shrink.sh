249c01e9d84eba607cc232ca3814c9d5a26db521
loomanss <stijnloomans@live.be>
2021-06-29 18:57:45 +0200

    Update ubuntu_shrink.sh

--- ubuntu_shrink.sh
+++ ubuntu_shrink.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # script to remove packages from 20.04 desktop
 
+message() { echo -e "\e[1;32m\n${1}\n\e[0m"; }
+warning() { echo -e "\e[1;33m\nWarning: ${1}\n\e[0m"; }
+error()   { echo -e "\e[1;31m\nError: ${1}\n\e[0m";  exit 1; }
+
+
+
 # wget -qO - https://raw.githubusercontent.com/loomanss/ubuntu-install/main/ubuntu_shrink.sh | bash
 
 
@@ -9,7 +15,7 @@
 sudo sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
 sudo update-grub
 
-
+message "setting autologon..."
 # automatic logon for Virtualbox-ubuntu
 
 cat > /tmp/set_autologon.sh << eof
@@ -22,32 +28,35 @@ chmod +x /tmp/set_autologon.sh
 /tmp/set_autologon.sh
 
 # disable sudo password 
-
+message "disable credential-check when sudoing ..."
 echo "$USER     ALL=(ALL) NOPASSWD:ALL" > /tmp/$USER
 sudo cp /tmp/$(echo $USER) /etc/sudoers.d/.
 
 
 # disable ip6 and smaller timeout for grub
-
+message "disable ip and bring down grub_timeout to s..."
 sudo sed -i -e 's/GRUB_TIMEOUT=10/GRUB_TIMEOUT=1/' /etc/default/grub
 sudo sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
 sudo update-grub
 
-
+message "apt update and install curl git jq..."
 sudo apt update -y && sudo apt -y upgrade
 sudo apt-get autoclean
 sudo apt-get autoremove 
 sudo apt install curl git jq -y
 
 
+message "remove gnome stuff...."
 sudo apt autoremove snapd gnome-software-plugin-snap speech-dispatcher-audio-plugins speech-dispatcher-espeak-ng  speech-dispatcher \
 whoopsie update-notifier update-notifier-common packagekit xserver-xorg-input-wacom libwacom-bin avahi-daemon gnome-bluetooth \
 gnome-power-manager powermgmt-base gnome-themes-extra-data gnome-themes-extra pulseaudio pulseaudio-utils xul-ext-ubufox \
 tracker tracker-extract tracker-miner-fs libmbim-proxy whoopsie bluez bluez-cups bluez-obexd  whoopsie-preferences \
-gnome-online-accounts gvfs-backends apport apport-gtk apport-symptoms ubuntu-advantage-tools gnome-font-viewer \
+gnome-online-accounts gvfs-backends apport apport-gtk apport-symptoms  gnome-font-viewer \
 gnome-calculator gnome-getting-started-docs  -y
 
+# ubuntu-advantage-tools
 
+message "remove printer stuff..."
 # remove printer drivers
 sudo apt autoremove printer-driver-m2300w printer-driver-splix printer-driver-postscript-hp printer-driver-pxljr printer-driver-min12xxw \ 
 printer-driver-pnm2ppa printer-driver-foo2zjs printer-driver-brlaser printer-driver-c2esp printer-driver-ptouch  printer-driver-c2esp \
@@ -56,12 +65,25 @@ cups-browsed cups-filters-core-drivers cups -y
 
 # kernel cleanup
 
+message "kernel cleanup..."
 dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y purge
 
+
+
 # make it possible to disable uneeded startup apps
-sudo sed -i 's/NoDisplay=true/NoDisplay=false/g' /etc/xdg/autostart/*.desktop
+
+message "disable autostart for default stuff. start 'gnome-session-properties' to configure .... "
+mkdir -p ~/.config/autostart/
+sudo sed -i -e 's/NoDisplay=true/NoDisplay=false/' /etc/xdg/autostart/*
+cp /etc/xdg/autostart/* ~/.config/autostart/
+sudo sed -i -e 's/X-GNOME-Autostart-enabled=true/X-GNOME-Autostart-enabled=false/' ~/.config/autostart/*
+sudo sed -i -e 's/NoDisplay=true/NoDisplay=false/' ~/.config/autostart/*
+sudo sed -i -e 's/X-GNOME-AutoRestart=true/X-GNOME-AutoRestart=false/' ~/.config/autostart/*
+sudo sed -i -e 's/X-GNOME-Autostart-Notify=true/X-GNOME-Autostart-Notify=false/' ~/.config/autostart/*
 
 
+
+message "Disabling services... "
 systemctl list-unit-files --state=enabled | wc -l
 sudo systemctl mask  bluetooth.service
 systemctl --reverse list-dependencies cups.service
@@ -100,17 +122,20 @@ sudo systemctl mask whoopsie.service
 systemctl list-unit-files --state=enabled | wc -l
 
 # disable animations and  background image to save mem
+message "setting default ui options to save memory..."
 gsettings set org.gnome.desktop.background picture-options 'none'
 gsettings set org.gnome.desktop.background primary-color '#000455'
 gsettings set org.gnome.desktop.interface enable-animations false
 
 # other ui stuff
+message "setting other ui options..."
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.shell.overrides attach-modal-dialogs false  # modals don't "stick" to parent window
 # gsettings set org.gnome.shell enable-hot-corners true
 gsettings set org.gnome.settings-daemon.plugins.print-notifications active false
 
 # disable some hardware-stuff not needed in virtualbox
+message "disabling hardware..."
 gsettings set org.gnome.settings-daemon.plugins.gsdwacom active false
 gsettings set org.gnome.settings-daemon.plugins.housekeeping active false
 gsettings set org.gnome.settings-daemon.plugins.smartcard active false
@@ -127,6 +152,7 @@ gsettings set org.gnome.settings-daemon.plugins.keyboard active false
 
 # gsettings set org.gtk.Settings.FileChooser show-hidden true
 # Experimental - start
+message "Experimental - start..."
 sudo mv /usr/lib/evolution-data-server /usr/lib/evolution-data-server-disabled
 sudo mv /usr/lib/evolution-addressbook-factory /usr/lib/evolution-addressbook-factory-disabled
 sudo mv /usr/libexec/evolution-calendar-factory /usr/libexec/evolution-calendar-factory-disabled
@@ -134,7 +160,9 @@ sudo mv /usr/libexec/evolution-source-registry /usr/libexec/evolution-source-reg
 sudo mv /usr/libexec/evolution-data-server/evolution-alarm-notify /usr/libexec/evolution-data-server/evolution-alarm-notify-disabled
 # Experimental - end
 
+message "journalctl optimalization..."
 # journalctl optimalization
 sudo journalctl --vacuum-size=300M --vacuum-time=2d --vacuum-files=5
 #sudo rm /var/log/ -r
 
+message "Done!"
