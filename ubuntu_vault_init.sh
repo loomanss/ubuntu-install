51d65b7c78613b2dd080ee7ab57a85817ac135d5
loomanss <stijnloomans@live.be>
2021-06-29 15:35:33 +0200

    Update ubuntu_vault_init.sh

--- ubuntu_vault_init.sh
+++ ubuntu_vault_init.sh
@@ -5,7 +5,7 @@ cd /tmp/
 
 wget -O vault.zip  "https://vault.bitwarden.com/download/?platform=linux&app=cli"
 unzip vault.zip
-sudo install bw /usr/local/bin/
+sudo install bw /usr/local/bin/ 
 
 echo ' start usering bw with  export BW_SESSION=$(bw login <emailaddress> --raw) '
 echo "bw get item http://url.com wil return full object. get the id"
