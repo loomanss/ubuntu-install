3c3886f91d966a50a1fa55f480518d97a0533aaf
loomanss <stijnloomans@live.be>
2021-06-29 13:06:22 +0200

    Update ubuntu_vault_init.sh

--- ubuntu_vault_init.sh
+++ ubuntu_vault_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# wget -qO "https://raw.githubusercontent.com/loomanss/ubuntu-install/main/ubuntu_vault_init.sh" | bash 
+# wget -qO - https://raw.githubusercontent.com/loomanss/ubuntu-install/main/ubuntu_vault_init.sh | bash 
 cd /tmp/
 wget -O vault.zip  "https://vault.bitwarden.com/download/?platform=linux&app=cli"
 unzip vault.zip
@@ -10,5 +10,4 @@ echo " start usering bw with  export BW_SESSION=$(bw login <emailaddress> --raw)
 echo "bw get item http://url.com wil return full object. get the id"
 echo "bw get password id wil return only password"
 
-read MAILUSER
-export BW_SESSION=$(bw login $MAILUSER --raw)
+
