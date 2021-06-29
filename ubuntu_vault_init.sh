0c7fe13d59d2cc42dd4efb952d3f2ad34131d6dd
loomanss <stijnloomans@live.be>
2021-06-29 19:04:43 +0200

    Update ubuntu_vault_init.sh

--- ubuntu_vault_init.sh
+++ ubuntu_vault_init.sh
@@ -35,15 +35,15 @@ export BW_SESSION=$(bw unlock --raw)
 
 echo "installing ssh keys ..."
 bw list items | jq -c '.[] |select(.id|contains("53d2339c-70a1-4ed3-a077-aaf100a470bd")) | .notes' >> ~/.ssh/id_rsa.b64
- sed -ie 's/^"//'  ~/.ssh/id_rsa.b64
- sed -ie  's/"$//' ~/.ssh/id_rsa.b64
+sed -ie 's/^"//'  ~/.ssh/id_rsa.b64
+sed -ie 's/"$//' ~/.ssh/id_rsa.b64
 openssl base64  -A -d -in ~/.ssh/id_rsa.b64 -out ~/.ssh/id_rsa
 cat  ~/.ssh/id_rsa
 
 
 bw list items | jq -c '.[] |select(.id|contains("8e383309-94f8-4fbb-b381-ab4400caaf33")) | .notes' >> ~/.ssh/id_rsa.pub.b64
- sed -ie 's/^"//'  ~/.ssh/id_rsa.pub.b64
- sed -ie  's/"$//' ~/.ssh/id_rsa.pub.b64
+sed -ie 's/^"//'  ~/.ssh/id_rsa.pub.b64
+sed -ie 's/"$//' ~/.ssh/id_rsa.pub.b64
 openssl base64  -A -d -in ~/.ssh/id_rsa.pub.b64 -out ~/.ssh/id_rsa.pub
 cat  ~/.ssh/id_rsa.pub
 
