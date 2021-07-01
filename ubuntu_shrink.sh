86f32a2f108f9d27212d4d3ecc0ba719c587c477
loomanss <stijnloomans@live.be>
2021-07-01 12:12:32 +0200

    Update ubuntu_shrink.sh

--- ubuntu_shrink.sh
+++ ubuntu_shrink.sh
@@ -188,7 +188,8 @@ sudo mv /usr/libexec/evolution-data-server/evolution-alarm-notify /usr/libexec/e
 # snd_pcsp: low-quality, just noise when being used for sound playback, causes  hangs at desktop session start (Ubuntu: #246969)
 # amd76x_edac: EDAC driver for amd76x clashes with the agp driver preventing the aperture from being initialised (Ubuntu: #297750). Blacklist so that the driver continues to build and is installable for the few cases where its really needed.
 # uvcvideo: Disable webcam
-modules=("btusb" "uvcvideo")
+# parport_pc : disable parallel port
+modules=("btusb" "uvcvideo" "parport_pc")
 
 for MOD in "${modules[@]}"; do
   CURRENT_CMD="sudo echo 'blacklist $MOD' | sudo tee /etc/modprobe.d/blacklist-$MOD.conf"
